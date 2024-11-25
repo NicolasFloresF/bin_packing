fa6d278d9a28f5fadf73f7ea55e928333dc0d6df
NicolasFloresF <nick.lasff@gmail.com>
2024-11-25 13:38:48 -0300

    [UPDATE] Code cleanup and docstring update

--- bin_packing/BinPackingGA.py
+++ bin_packing/BinPackingGA.py
@@ -3,49 +3,74 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def random_sampling_generator(items, binsCapacity, sampleSize):
+def random_sampling_generator(items: list, binsCapacity: list, sampleSize: int):
+    """Generates a random sample of solutions for the bin packing problem
+
+    Args:
+        items (list): List of items to be packed
+        binsCapacity (list): List of bins capacity
+        sampleSize (int): Number of solutions to be generated
+
+    Returns:
+        list: List of solutions generated and their respective bins current capacity
+    """
     sample = []
     sampleBinsCurCapacity = []
+
+    # iterate over the sample size
     for _ in range(sampleSize):
         solution = []
         binsCurCapacity = [0] * len(binsCapacity)
         for j in range(len(items)):
+            # try to find a bin that can hold the item 100 times
             for _ in range(100):
                 randNum = random.randint(0, len(binsCapacity) - 1)
                 if binsCurCapacity[randNum] + items[j] > binsCapacity[randNum]:
                     randNum = random.randint(0, len(binsCapacity) - 1)
                 else:
                     break
+
+            # append the bin number to the solution
             solution.append(randNum)
             binsCurCapacity[solution[j]] += items[j]
 
+        # append the solution and the bins current capacity to the sample
         sampleBinsCurCapacity.append(binsCurCapacity)
         sample.append(solution)
+
     return sample, sampleBinsCurCapacity
 
 
 def overloadPenalty(binsCurCapacity: list, binsCapacity: list):
+    """Calculates the overload penalty for the current bins capacity
+
+    Args:
+        binsCurCapacity (list): List of bins current capacity
+        binsCapacity (list): List of bins capacity
+
+    Returns:
+        int: Overload penalty value
+    """
     overload = 0
     for i in range(len(binsCapacity)):
         overPercentage = binsCurCapacity[i] / binsCapacity[i]
         if overPercentage <= 1:
             overPercentage = 0
-        # print(overPercentage)
         overload += max(0, overPercentage)
     return overload
 
 
 def fitness_function(binsCurCapacity: list, binsCapacity: list):
-    # The Fitness Function implementation is based on the following article https://www.codeproject.com/Articles/633133/Genetic-Algorithm-for-Bin-Packing-Problem
-    # F = Σⁿᵢ[(fᵢ/cᵢ)ᵏ]/n
-    # Where:
-    # F - fitness of the solution
-    # n - number of bins
-    # fi - fill of the ith bin
-    # c - capacity of the bin
-    # k - constant greater then 1
-    # constant controls whether the more filled bins are preferred or equally filled bins. Larger values should be used in case more filled bins are preferred. This example sets value of k to 2.
-    k = 3
+    """Calculates the fitness value for a solution
+
+    Args:
+        binsCurCapacity (list): List of bins current capacity
+        binsCapacity (list): List of bins capacity
+
+    Returns:
+        float: Fitness value
+    """
+    k = 2
     sum = 0
     bins = 0
     for i in range(len(binsCapacity)):
@@ -53,21 +78,28 @@ def fitness_function(binsCurCapacity: list, binsCapacity: list):
             bins += 1
         sum += (binsCurCapacity[i] / binsCapacity[i]) ** k
     fitness = sum / max(1, bins)  # len(binsCapacity)
-    # 0   0  5  4 13  7  4 10  5
-    # 10 10 15 10 15 12 10 10 10
-    # 0/7
-    # 0 - 1
-    # -1
-    # 1
-    # 0 0 0 0 0 0 8 10 12
-    #! implent overload penalty
+
+    # put the max fitness value to 1 (invalid solution can have a fitness value greater than 1)
     fitness = min(1, fitness)
+
+    # apply the overload penalty
     fitness /= max(1, overloadPenalty(binsCurCapacity, binsCapacity))
-    # print(binsCurCapacity, overloadPenalty(binsCurCapacity, binsCapacity))
+
     return fitness
 
 
-def pointCrossOver(parent1, parent2, binsCapacity, items):
+def pointCrossOver(parent1: list, parent2: list, binsCapacity: list, items: list):
+    """Performs a point crossover between two parents
+
+    Args:
+        parent1 (list): First parent solution
+        parent2 (list): Second parent solution
+        binsCapacity (list): List of bins capacity
+        items (list): List of items to be packed
+
+    Returns:
+        list, list: Child solution and bins current capacity
+    """
     binsCurCapacity = [0] * len(binsCapacity)
     child = []
     for i in range(len(parent1)):
@@ -81,28 +113,27 @@ def pointCrossOver(parent1, parent2, binsCapacity, items):
     return child, binsCurCapacity
 
 
-def UniformCrossOver(parent1, parent2, binsCapacity, items):
+def UniformCrossOver(parent1: list, parent2: list, binsCapacity: list, items: list):
+    """Performs a uniform crossover between two parents
+
+    Args:
+        parent1 (list): First parent solution
+        parent2 (list): Second parent solution
+        binsCapacity (list): List of bins capacity
+        items (list): List of items to be packed
+
+    Returns:
+        list, list: Child solution and bins current capacity
+    """
     binsCurCapacity = [0] * len(binsCapacity)
     child = []
     for i in range(len(parent1)):
-        # flag = False
         randomNum = random.random()
-
-        # try to add the item to the bin of the first parent
-        # if binsCurCapacity[parent1[i]] + items[i] <= binsCapacity[parent1[i]]:
-        #     child.append(parent1[i])
-        #     binsCurCapacity[parent1[i]] += items[i]
-        # elif randomNum < 0.5 and binsCurCapacity[parent2[i]] + items[i] <= binsCapacity[parent2[i]]:
-        #     child.append(parent2[i])
-        #     binsCurCapacity[parent2[i]] += items[i]
-        # else:
-        #     flag = True
 
         if randomNum < 0.5:
             child.append(parent1[i])
             binsCurCapacity[parent1[i]] += items[i]
 
-        # try to add the item to the bin of the second parent
         if randomNum > 0.5:
             child.append(parent2[i])
             binsCurCapacity[parent2[i]] += items[i]
@@ -110,7 +141,18 @@ def UniformCrossOver(parent1, parent2, binsCapacity, items):
     return child, binsCurCapacity
 
 
-def singlePointMutation(child, binsCapacity, items, binsCurCapacity):
+def singlePointMutation(child: list, binsCapacity: list, items: list, binsCurCapacity: list):
+    """Performs a single point mutation in the child solution
+
+    Args:
+        child (list): Child solution
+        binsCapacity (list): List of bins capacity
+        items (list): List of items to be packed
+        binsCurCapacity (list): List of bins current capacity
+
+    Returns:
+        list, list: Mutated child solution and bins current capacity
+    """
     for i in range(len(child)):
         if random.random() < 0.2:
             randNum = random.randint(0, len(binsCapacity) - 1)
@@ -118,44 +160,53 @@ def singlePointMutation(child, binsCapacity, items, binsCurCapacity):
             binsCurCapacity[randNum] += items[i]
             child[i] = randNum
 
-    # checkCurCapacity(items, binsCapacity, child)
     return child, binsCurCapacity
 
 
-def tournamentSelection(sample, fitness_list):
-    # calculate the probability of each solution to be chosen
-    p = np.asarray(fitness_list).astype("float64")
-    p /= np.sum(p)
+def tournamentSelection(sample: list, fitness_list: list):
+    """Performs a tournament selection to choose the parents for the next generation
 
-    # print(p)
+    Args:
+        sample (list): List of solutions
+        fitness_list (list): List of fitness values for each solution
+
+    Returns:
+        list: List of chosen parents
+    """
+
+    # calculate the probability of each solution to be chosen
+    probability = np.asarray(fitness_list).astype("float64")
+    probability /= np.sum(probability)
 
     # print("Chosen solutions:")
-    parents = np.random.choice(len(sample), 50, p=p)
+    parents = np.random.choice(len(sample), 50, p=probability)
 
     return parents
 
 
+def generateRandomProblem(n, m):
+    """Generates a random problem for the bin packing problem
+
+    Args:
+        n (int): number of items to be generated
+        m (int): number of bins to be generated
+
+    Returns:
+        list, list: List of items and list of bins capacity
+    """
+    items = []
+    for i in range(n):
+        items.append(random.randint(1, 10))
+    binsCapacity = []
+    for i in range(m):
+        binsCapacity.append(random.randint(10, 20))
+    return items, binsCapacity
+
+
 def main():
-    # inputs
-    # items = [5, 5, 4, 7, 1, 3]
     items = [7, 5, 1, 5, 3, 4, 5, 10, 8, 3, 2, 5, 1, 4, 3, 7, 4]
     binsCapacity = [10, 10, 15, 10, 15, 12, 10, 10, 10]
 
-    # output
-    # solution = [0, 1, 2, 2, 0, 1]
-
-    # binsCurCapacity = checkCurCapacity(items, binsCapacity, solution)
-
-    # print(solution, binsCurCapacity)
-    # print(fitness_function(binsCurCapacity, binsCapacity))
-    #
-    # solution, binsCurCapacity = first_fit(items, binsCapacity)
-    #
-    # print(solution, binsCurCapacity)
-    # print(fitness_function(binsCurCapacity, binsCapacity))
-
-    #! invalid solution
-    #! duplicated or similar solutions (heuristic solutions? - first fit)
     random.seed(1)
     sample, sampleBinsCurCapacity = random_sampling_generator(items, binsCapacity, 50)
 
@@ -163,14 +214,10 @@ def main():
     for i in range(len(sampleBinsCurCapacity)):
         fitness_list.append(fitness_function(sampleBinsCurCapacity[i], binsCapacity))
 
-    # for i in range(len(sample)):
-    #     print(fitness_list[i], sample[i], sampleBinsCurCapacity[i])
-
-    # print("-------------------------------------")
-    # x and y axis plot creation
     x = []
     bestFit = []
     worstFit = []
+    medianFit = []
     for gen in range(5000):
         if fitness_list[0] == 1:
             break
@@ -178,38 +225,15 @@ def main():
         parents = tournamentSelection(sample, fitness_list)
 
         for i in range(int(len(parents) / 2)):
-            child, bin = UniformCrossOver(sample[parents[i]], sample[parents[i + 1]], binsCapacity, items)
+            child, bin = pointCrossOver(sample[parents[i]], sample[parents[i + 1]], binsCapacity, items)
             child, bin = singlePointMutation(child, binsCapacity, items, bin)
             childFit = fitness_function(bin, binsCapacity)
-            # print(child, bin, childFit)
             sample.append(child)
             sampleBinsCurCapacity.append(bin)
             fitness_list.append(childFit)
 
-        # child, bin = UniformCrossOver(sample[parents[0]], sample[parents[1]], binsCapacity, items)
-        # print("uniform crossover")
-        # print(child, bin)
-        # print(fitness_function(bin, binsCapacity))
-        # if child is not None:
-        #     child, bin = mutation(child, binsCapacity, items, bin)
-        #     print("mutation")
-        #     print(child, bin)
-        #     childFit = fitness_function(bin, binsCapacity)
-        #     print(childFit)
-
-        # fitness_list.append(childFit)
-        # sample.append(child)
-        # sampleBinsCurCapacity.append(bin)
-
-        # print(len(fitness_list), len(sample), len(sampleBinsCurCapacity))
-
-        # # zip the fitness values with the sample and sort them in descending order
-        # #!important: this is the right zip order: fitness_list, sample, sampleBinsCurCapacity
-
         zipped_lists = zip(fitness_list, sample, sampleBinsCurCapacity)
         sorted_zipped_lists = sorted(zipped_lists, reverse=True)
-        # fitness_list, sample, sampleBinsCurCapacity = zip(*sorted_zipped_lists)
-        # unzip with list comprehension
         fitness_list, sample, sampleBinsCurCapacity = [list(t) for t in zip(*sorted_zipped_lists)]
 
         while len(sample) > 50:
@@ -228,21 +252,14 @@ def main():
             sampleBinsCurCapacity[-1],
             overloadPenalty(sampleBinsCurCapacity[-1], binsCapacity),
         )
-        # plot data insertion
+
         x.append(gen)
         bestFit.append(fitness_list[0])
+        medianFit.append(fitness_list[int(len(fitness_list) / 2)])
         worstFit.append(fitness_list[-1])
 
-    # print(len(fitness_list), len(sample), len(sampleBinsCurCapacity))
-
-    # print the fitness values for each solution in the sample
-    # for i in range(len(sample)):
-    #     print(fitness_list[i], sample[i], sampleBinsCurCapacity[i])
-
-    # print(fitness_list)
-    # print(sample)
-    # print(sampleBinsCurCapacity)
     plt.plot(x, bestFit, label="Best Fitness")
+    plt.plot(x, medianFit, label="Median Fitness")
     plt.plot(x, worstFit, label="Worst Fitness")
     plt.legend()
     plt.xlabel("Generations")
